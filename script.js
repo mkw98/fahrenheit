8382f77c7922be2b7052d2957079f63820c0112e
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-31 11:42:33 +0100

    fix syntax error

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ button2.addEventListener('click', function(){
         output2.innerHTML = 'Wpisz tylko cyfry' + '<br><br>';
       } else if (fahrenheit2 === '') {
         output2.innerHTML = 'Nic nie wpisano' + '<br><br>';
-	else if (fahrenheit2 < -459.67) {
+      } else if (fahrenheit2 < -459.67) {
           output2.innerHTML = ' '+fahrenheit2+' stopni Fahrenheita? Taka temperatura nie jest fizycznie możliwa' + '<br><br>';
       } else {
         celsius2 = FtoC(fahrenheit2);
