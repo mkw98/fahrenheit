38b7eb946aa35e3f1f233f769c9294489d7c0c40
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-31 11:09:02 +0100

    fix typo

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 'use strict';
-//Funkcja, która przyjmuje temperaturę C i zwraca temperaturę F.
-//Funkcja, która przyjmuje temperaturę C i zwraca informację np. o stanie skupienia wody.
-//Funkcja, która przyjmuje temperaturę F i zwraca temperaturę C.
-//Funkcję, która wyświetla tekst na stronie, odpowiednio dodając nową linię.
+
+//Dodaj funkcję, która wyświetla tekst na stronie, odpowiednio dodając nową linię.
 // \n` dodaje nową linię
-//var square = function(x) {
-//  return x * x;
-//}
-// wersja alternatywna
-// function square(x) {
-//   return x * x;
-// }
 
-//var a = prompt("Podaj liczbę");
-//var result = square(a);
-//console.log(result);
-
-//function convertCelToFah(deg) {
-//return (5/9) * (deg - 32);
-//}
-//
-
-//Kursanci muszą się nauczyć programować na zasadach SOLID, gdzie pierwsza z liter oznacza Single Responsibility Principle (Zasada pojedynczej odpowiedzialności). Funkcja / Metoda / Klasa powinna wykonywać jedno określone zadanie.
-//W naszym przypadku funkcja ma wyliczyć ile stopni Celsjusza to Fahrenheita. .
+var output = document.getElementById('greeter-output');
+var output3 = document.getElementById('greeter-output3');
+var output4 = document.getElementById('greeter-output4');
+output.innerHTML = 'Kliknij i podaj temperaturę w stopniach Celsjusza' + '<br><br>' + output.innerHTML; 
+var button = document.getElementById('greeter-button');
+var celsius;
+var fahrenheit;
+var button2 = document.getElementById('greeter-button2');
+var output2 = document.getElementById('greeter-output2');
+var celsius2;
+var fahrenheit2;
 
 //Funkcja, która przyjmuje temperaturę C i zwraca temperaturę F.
 function CtoF(x) {
@@ -49,17 +40,6 @@ function water(x) {if  (x <= 0 && x >= -273.15) {
 }
 //Funkcję, która wyświetla tekst na stronie, odpowiednio dodając nową linię.
 
-var output = document.getElementById('greeter-output');
-var output3 = document.getElementById('greeter-output3');
-var output4 = document.getElementById('greeter-output4');
-
-output.innerHTML = 'Kliknij i podaj temperaturę w stopniach Celsjusza' + '<br><br>' + output.innerHTML; 
-
-var button = document.getElementById('greeter-button');
-
-var celsius;
-var fahrenheit;
-
 button.addEventListener('click', function(){
 	
 	celsius = window.prompt('Podaj, proszę temperaturę w stopniach Celsjusza');
@@ -73,7 +53,7 @@ if (isNaN(celsius))  {
   else if(celsius === '') {
     output.innerHTML = 'Nic nie wpisano' + '<br><br>';
   }
-  else if ( (celsius < -273.15) {
+  else if (celsius < -273.15) {
     output.innerHTML = ' '+celsius+' stopni Celsjusza? Taka temperatura nie jest fizycznie możliwa' + '<br><br>';
   }
   else {
@@ -83,11 +63,6 @@ if (isNaN(celsius))  {
     }
 });
    
-var button2 = document.getElementById('greeter-button2');
-var output2 = document.getElementById('greeter-output2');
-var celsius2;
-var fahrenheit2;
-
 output2.innerHTML = 'Kliknij i podaj temperaturę w stopniach Fahrenheita' + '<br><br>' + output2.innerHTML; 
 button2.addEventListener('click', function(){
 	
@@ -106,8 +81,7 @@ button2.addEventListener('click', function(){
       } else {
         celsius2 = FtoC(fahrenheit2);
         if (celsius2 <= 0 && celsius2 > -273.15) {
-           output2.innerHTML = ' '+fahrenheit2+' stopni Fahrenheita to '+celsius2+' stopni Celsjusza! W tej temperaturze woda przyjmuje postać lodu' + '<br><br>';
-        
+           output2.innerHTML = ' '+fahrenheit2+' stopni Fahrenheita to '+celsius2+' stopni Celsjusza! W tej temperaturze woda przyjmuje postać lodu' + '<br><br>'; 
         } else if (celsius2 > 0 && celsius2 <100 ) {
           output2.innerHTML = ' '+fahrenheit2+' stopni Fahrenheita to '+celsius2+' stopni Celsjusza! W tej temperaturze woda jest cieczą' + '<br><br>'+ output2.innerHTML;
         } else if (celsius2 >= 100) {
